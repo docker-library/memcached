2b488f8e93867964765e837a1198a11d79ec1d97
Joe Ferguson <joe@infosiftr.com>
2014-12-31 14:24:57 -0800

    Add update and generate-stackbrew scripts

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+fullVersion="$(curl -sSL 'http://memcached.org/files/' | grep -E '<a href="memcached-[0-9.]+\.tar\.gz"' | sed -r 's!.*<a href="memcached-([0-9.]+)\.tar\.gz".*!\1!' | sort -V | tail -1)"
+
+sha1="$(curl -sSL "http://memcached.org/files/memcached-$fullVersion.tar.gz.sha1" | cut -d' ' -f1)"
+
+set -x
+sed -ri '
+	s/^(ENV MEMCACHED_VERSION) .*/\1 '"$fullVersion"'/;
+	s/^(ENV WORDPRESS_SHA1) .*/\1 '"$sha1"'/;
+' Dockerfile
