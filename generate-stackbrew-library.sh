b051a70f217efefecbb43a3d028a5f6e3e1c4080
Tianon Gravi <admwiggin@gmail.com>
2014-12-31 22:27:23 -0700

    Protect against branches named "." in generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,7 +7,7 @@ url='git://github.com/docker-library/memcached'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' .)"
+commit="$(git log -1 --format='format:%H' -- .)"
 fullVersion="$(grep -m1 'ENV MEMCACHED_VERSION ' Dockerfile | cut -d' ' -f3)"
 
 versionAliases=()
