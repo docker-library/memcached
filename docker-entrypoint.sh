236cad399df7013361283b10365942f1c6bd1b81
Natanael Copa <ncopa@alpinelinux.org>
2016-03-24 13:42:30 +0100

    use sh for instead of bash for docker-entrypoint.sh
    Avoid use bash so we can be consistent with alpine image.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # first check if we're passing flags, if so
 # prepend with memcached
-if [ "${1:0:1}" = '-' ]; then
-	set -- memcached "$@"
-fi
+case "$1" in
+-*) set -- memcached "$@";;
+esac
 
 exec "$@"
