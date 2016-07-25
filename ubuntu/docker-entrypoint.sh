5d838e5f22e4d48ac7da389f415c020f87d2bdee
Oleg Abrosimov <olegabr@yandex.ru>
2016-07-26 01:12:35 +0700

    fix -l usage

--- ubuntu/docker-entrypoint.sh
+++ ubuntu/docker-entrypoint.sh
@@ -6,9 +6,9 @@ CONNECTIONS=${MEMCACHED_CONNECTIONS:-1024}
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- memcached -m ${MEMORY} -c ${CONNECTIONS} -l "/var/log/memcached.log" "$@"
+	set -- memcached -m ${MEMORY} -c ${CONNECTIONS} "$@"
 else
-	set -- memcached -m ${MEMORY} -c ${CONNECTIONS} -l "/var/log/memcached.log"
+	set -- memcached -m ${MEMORY} -c ${CONNECTIONS}
 fi
 
 exec "$@"
