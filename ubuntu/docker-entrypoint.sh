1791b6896e0bf6cc8e59aeefa51b1de9f0db13f3
Oleg Abrosimov <olegabr@yandex.ru>
2016-07-25 16:44:33 +0700

    set log file explicitly
    to match the path prepared in the Dockerfile

--- ubuntu/docker-entrypoint.sh
+++ ubuntu/docker-entrypoint.sh
@@ -6,9 +6,9 @@ CONNECTIONS=${MEMCACHED_CONNECTIONS:1024}
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- memcached -m ${MEMORY} -c ${CONNECTIONS} "$@"
+	set -- memcached -m ${MEMORY} -c ${CONNECTIONS} -l "/var/log/memcached.log" "$@"
 else
-	set -- memcached -m ${MEMORY} -c ${CONNECTIONS}
+	set -- memcached -m ${MEMORY} -c ${CONNECTIONS} -l "/var/log/memcached.log"
 fi
 
 exec "$@"
